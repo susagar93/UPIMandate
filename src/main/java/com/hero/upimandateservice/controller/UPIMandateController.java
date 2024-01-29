4b602f1d4268805a9fe0732360f9d842b44290e8
surendras <surendra.sagar@herofincorp.com>
2024-01-29 15:13:40 +0530

    added swagger document code

--- src/main/java/com/hero/upimandateservice/controller/UPIMandateController.java
+++ src/main/java/com/hero/upimandateservice/controller/UPIMandateController.java
@@ -4,10 +4,11 @@ package com.hero.upimandateservice.controller;
 import com.hero.upimandateservice.exception.UPIMandateResponseException;
 import com.hero.upimandateservice.model.APILogger;
 import com.hero.upimandateservice.model.UPIMandateRequest;
-import com.hero.upimandateservice.model.VerifyVPARequest;
 import com.hero.upimandateservice.service.impl.UPIMandateCreationServiceImpl;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/api")
-//@Tag(name = "UPIMandateController" ,description = "This is UPI Mandate API")
+@Tag(name = "UPIMandateController" ,description = "This is UPI Mandate API")
 public class UPIMandateController {
 
     private static final Integer BAD_REQUEST = 400;
@@ -35,6 +36,11 @@ public class UPIMandateController {
     }
 
     @PostMapping(value = "/upi/mandate",produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "create mandate api" , description = "This is api to call create mandate")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "request is not valid")
+    })
     public ResponseEntity<String> createMandate(@RequestBody UPIMandateRequest upiMandateRequest) {
         APILogger log = new APILogger("/upi/mandate", upiMandateRequest.getAppID());
         String mandate;
--- src/main/java/com/hero/upimandateservice/swagger/SwaggerConfig.java
+++ src/main/java/com/hero/upimandateservice/swagger/SwaggerConfig.java
@@ -1,17 +1,26 @@
 package com.hero.upimandateservice.swagger;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
 
-//    @Bean
-//    public OpenAPI springShopOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info().title("UPI Mandate API")
-//                        .description("This is UPI Mandate API ::  UPI Autopay Mandate")
-//                        .version("1.0")
-//                        .contact(new Contact().name("HERO FIN CORP LTD"))
-//                        .license(new License().name("Apache")))
-//                .externalDocs(new ExternalDocumentation().description("").url(""));
-//
-//
-//    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("UPI Mandate API")
+                        .description("This is UPI Mandate API ::  UPI Autopay Mandate")
+                        .version("1.0")
+                        .contact(new Contact().name("HERO FIN CORP LTD"))
+                        .license(new License().name("Apache")))
+                .externalDocs(new ExternalDocumentation().description("").url(""));
+
+
+    }
 }
